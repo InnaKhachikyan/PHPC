f983c519b831d828486d19e61a47a039ca817b3e
Inna Khachikyan <khachikyaninna@gmail.com>
2025-04-03 21:49:45 +0400

    done with OMP method

---
+++ homework5/README.md
@@ -0,0 +1,20 @@
+There is fibonacciDP method which is a dynamic programming method. I created that just for checking my omp method calculations are correct.
+I don't use the standard recursive method (fibonacciRec) for checking as it is very slow, I just wrote it to visualize what should be made parallel.
+
+fibonacciOmp method
+In the method there is a region parallel which creats multiple threads to execute the parallel region. Inside the parallel region one thread calls the fibonacciTask recursive method with the argument n. single omp ensures that only one thread makes that call.
+When that single thread calls that method, it may either go to calculate F(num) iteratively in the base case if n<=10 or if n>10 it will go further to create tasks.
+At first the task is created by the single thread that called the recursive function.
+Let's say n = 20;
+It reaches the task shared(fib1) part, creates a task for n = 19 and n = 18, adds to the tasks queue. Some thread, let's say thread number 5, takes the task (F19) from the queue makes the recursive call fibonaccitask(19) enters the method, skips the base case and goes further creates the tasks for n = 18 and n = 17. 
+The task creation is done recursively, so the threads that took the task from the queue recursively create tasks for smalled n and add those to queue.
+So, the thread creating the task is not always the same (it's the thread that took the next task from the pool and reached the line of createing a new task). 
+The tasks are added to a queue and the next task in the queue is executed by any available thread at the moment.
+
+shared(fib1) and shared(fib2)
+this part ensures that fib1 and fib2 are shared variables, so the parent thread that created the tasks for n-1 and n-2 will have access to the final calculations made by other threads that took the tasks and calculated fib1 and fib2.
+
+taskwait
+this part ensures threads synchronization, we wait for the two threads executing n-1 and n-2 to finish their calculations and give the updated fib1 and fib2 so that the parent thread may calculate their sum.
+
+usleep and printings are added just for better visualization of the execution.
--- homework5/fibonacci.c
+++ homework5/fibonacci.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-long result;
+long fibonacciRec(int n);
 long fibonacciDP(int n);
 long fibonacciOmp(int n);
 long fibonacciTask(int n);
@@ -11,29 +11,17 @@ long fibonacciTask(int n);
 int main() {
 
 	long resultDP, resultOmp;
-	resultDP = fibonacciDP(10);
-	printf("FINAL RESULT DP n=10: %ld\n", resultDP);
-	resultOmp = fibonacciOmp(10);
-/*	#pragma omp parallel 
-	{ 
-		#pragma omp single
-		{
-			resultOmp = fibonacciOmp(10);
-		}
-	}
-*/	printf("FINAL RESULT OMP n=10: %ld\n", resultOmp);
+	int n = 10;
+	resultDP = fibonacciDP(n);
+	printf("FINAL RESULT DP n = %d: %ld\n", n, resultDP);
+	resultOmp = fibonacciOmp(n);
+	printf("FINAL RESULT OMP n = %d: %ld\n", n, resultOmp);
 
-	resultDP = fibonacciDP(15);
-	printf("FINAL RESULT DP n=15: %ld\n", resultDP);
-	resultOmp = fibonacciOmp(15);
-/*	#pragma omp parallel 
-	{ 
-		#pragma omp single
-		{
-			resultOmp = fibonacciOmp(15);
-		}
-	}
-*/	printf("FINAL RESULT OMP n=15: %ld\n", resultOmp);
+	n = 15;
+	resultDP = fibonacciDP(n);
+	printf("FINAL RESULT DP n = %d: %ld\n", n, resultDP);
+	resultOmp = fibonacciOmp(n);
+	printf("FINAL RESULT OMP n = %d: %ld\n", n, resultOmp);
 
 
 	return 0;
@@ -44,16 +32,25 @@ long fibonacciDP(int n) {
     if (n == 0) return 0;
     if (n == 1) return 1;
 
-    long dp[n + 1];  // Array to store Fibonacci numbers
+    long dp[n + 1];  
     dp[0] = 0;
     dp[1] = 1;
 
     for (int i = 2; i <= n; i++) {
-        dp[i] = dp[i - 1] + dp[i - 2];  // Compute Fibonacci iteratively
+        dp[i] = dp[i - 1] + dp[i - 2];  
     }
 
     return dp[n];
 }
+
+long fibonacciRec(int n) {
+	if(n == 0) return 0;
+	if(n == 1) return 1;
+
+	long result = fibonacciRec(n-1) + fibonacciRec(n-2);
+	return result;
+}
+	
 
 long fibonacciOmp(int n) {
 	long finalResult;
@@ -61,6 +58,7 @@ long fibonacciOmp(int n) {
 	{
 		#pragma omp single
 		{
+			printf("Thread number %d calls recursive Fibonacci\n", omp_get_thread_num());
 			finalResult = fibonacciTask(n);
 		}
 	}
@@ -77,30 +75,29 @@ long fibonacciTask(int n) {
 			fib1 = fib2;
 			fib2 = result;
 		}
-		printf("IN BASE CASE result %ld, n is %d\n", result, n);
 		return result;
 	}
 
 	else {	
+		usleep(100000);
+		printf("Thread number %d creates task %d\n", omp_get_thread_num(), n-1);
 		#pragma omp task shared(fib1)
 		{
-			printf("N is %d\n", n);
-			fib1 = fibonacciOmp(n-1);
-			printf("FIB1 is %ld, n is %d\n", fib1, n);
-			printf("number of thread %d\n", omp_get_thread_num());
-			//usleep(100000);
+			printf("Thread number %d processes %d-th Fibonacci\n", omp_get_thread_num(), n);
+			fib1 = fibonacciTask(n-1);
+			usleep(100000);
 		}
+		usleep(100000);
+		printf("Thread number %d creates task %d\n", omp_get_thread_num(), n-2);
 		#pragma omp task shared(fib2)
 		{
-			printf("N is %d\n", n);
-			fib2 = fibonacciOmp(n-2);
-			printf("FIB2 is %ld, n is %d\n", fib2, n);
-			//usleep(100000);
+			printf("Thread number %d processes %d-th Fibonacci\n", omp_get_thread_num(), n);
+			fib2 = fibonacciTask(n-2);
+			usleep(100000);
 		}
 		#pragma omp taskwait
 		printf("RESULT %ld \n", (fib1 + fib2));
 		return fib1 + fib2;
-		//usleep(1000000);
 	}
 }
 
